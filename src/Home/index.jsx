32f5f8b8c9ed97059a36514baf4830a8d528f244
Guibs <guilhermeferreirabrandao969@gmail.com>
2025-09-06 15:25:17 -0300

    [ADD]textAnBannerInHome

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,25 +1,23 @@
 import banner from'../assets/banner.png'
-
+import './styles.css'
 
 function Home() {
   return (
     <>
-    <div>
-      <img className='img' src={banner} alt="Banner 01" />
-
-      <p>Navigating the digital landscape for success</p>
+    <div className='container'>
+      
+      <div className='textBox'> 
+      <p className='text1'>Navigating the digital landscape for success</p>
 
       <p>Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
 
-       <a className='button'> Book a consultation </a>
+       <a className='button2'> Book a consultation </a>
+       </div>
+       <div className='imgBox'>
+        <img src={banner} alt="Banner 01" />
+       </div>
 
-
-
-
-
-
-
-
+      
     </div>
     </>
   )
